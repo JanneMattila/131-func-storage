1fe779fa96825be32ca5d7dc48a45e0694ad0ad9
Janne Mattila <janne.mattila@hotmail.fi>
2020-09-12 15:13:37 +0300

    Implement basic upload

--- src/StorageFunc/StorageFunction.cs
+++ src/StorageFunc/StorageFunction.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
 using StorageFunc.Interfaces;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Azure;
 
 namespace StorageFunc
 {
@@ -20,21 +23,44 @@ namespace StorageFunc
         {
             log.LogInformation("Csv converter function processed a request.");
 
-            var request = await JsonSerializer.DeserializeAsync<FileRequest>(req.Body);
-            if (request is null ||
-                string.IsNullOrWhiteSpace(request.Name) ||
-                request.File is null)
+            string filename = req.Headers["Filename"];
+            string validity = req.Headers["Validity"];
+
+            if (string.IsNullOrEmpty(req.ContentType))
             {
-                return new BadRequestObjectResult(
-                    new
-                    {
-                        error = "Missing mandatory parameters."
-                    });
+                log.LogWarning("Content-Type is required header.");
+                return new BadRequestObjectResult("Content-Type is required header.");
             }
 
+            if (!req.ContentLength.HasValue)
+            {
+                log.LogWarning("Content-Length is required header.");
+                return new BadRequestObjectResult("Content-Length is required header.");
+            }
+
+            var contentType = req.ContentType;
+            //using var reader = new BinaryReader(req.Body);
+            //var imageData = reader.ReadBytes((int)req.ContentLength.Value);
+
+            var storageConnectionString = Environment.GetEnvironmentVariable("Storage");
+            var blobServiceClient = new BlobServiceClient(storageConnectionString);
+            var blobContainerClient = blobServiceClient.GetBlobContainerClient("files");
+
+            var container = await blobContainerClient.CreateIfNotExistsAsync(PublicAccessType.None);
+
+            var blobClient = blobContainerClient.GetBlobClient(filename);
+            var blob = await blobClient.UploadAsync(req.Body, new BlobUploadOptions()
+            {
+                HttpHeaders = new BlobHttpHeaders()
+                {
+                    ContentType = contentType
+                }
+            });
+
+            var uri = blobClient.Uri;
             return new OkObjectResult(new FileResponse()
             {
-                Uri = "",
+                Uri = uri.ToString(),
                 ValidUntil = DateTimeOffset.UtcNow.AddHours(1)
             });
         }
